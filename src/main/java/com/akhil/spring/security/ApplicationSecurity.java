077bb29e0bf6eeee6953d0c4dfdbaad2737164d6
Akhilesh <akhil10x5@gmail.com>
2018-12-10 23:32:02 +0530

    spring security

---
+++ src/main/java/com/akhil/spring/security/ApplicationSecurity.java
@@ -0,0 +1,22 @@
+package com.akhil.spring.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+
+  @Autowired
+  public void initialize(AuthenticationManagerBuilder builder, DataSource dataSource) throws Exception {
+    builder.jdbcAuthentication().dataSource(dataSource).withUser("dave")
+      .password("secret").roles("USER");
+    
+  }
+
+}
+
