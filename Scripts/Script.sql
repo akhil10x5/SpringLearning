fc4d410b19d32b9d826bb6ac2a2ecdc8428a5568
Akhilesh <akhil10x5@gmail.com>
2018-12-18 22:08:12 +0530

    Hello world v1

---
+++ Scripts/Script.sql
@@ -0,0 +1,11 @@
+
+insert into salgrade values (2,1201,1400);
+
+insert into salgrade values (3,1401,2000);
+
+insert into salgrade values (4,2001,3000);
+
+insert into salgrade values (5,3001,99999);
+
+
+select * from salgrade
---
+++ src/main/java/com/akhil/SpringLearning/SpringLearningApplication.java
@@ -0,0 +1,15 @@
+package com.akhil.SpringLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class SpringLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningApplication.class, args);
+	}
+
+}
+
