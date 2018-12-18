798647858be0adbfeff927cf27c443a948186381
Akhilesh <akhil10x5@gmail.com>
2018-12-18 22:38:07 +0530

    Inmemory authentication

--- src/main/java/com/akhil/SpringLearning/SpringSecurityConfig.java
+++ src/main/java/com/akhil/SpringLearning/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.akhil.SpringLearning;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,9 +10,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter
 {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		/*
+		 * You can also simply prefix {noop} to your passwords in order for the DelegatingPasswordEncoder 
+		 * use the NoOpPasswordEncoder to validate these passwords. Notice that NoOpPasswordEncoder is deprecated though, 
+		 * as it is not a good practice to store passwords in plain text.
+		 */
+		
+		auth.inMemoryAuthentication().withUser("test").password("{noop}test").roles("USER");
+	}
+	
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception
 	{
-		httpSecurity.authorizeRequests().anyRequest().permitAll();
+		httpSecurity.authorizeRequests().anyRequest().permitAll().and().httpBasic();
 	}
 }
