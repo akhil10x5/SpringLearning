bec2b3114df8de25127aab5f72a125835d6908be
Akhilesh <akhil10x5@gmail.com>
2019-05-13 14:41:53 +0530

    Paging Demo

--- src/main/java/com/akhil/springlearning/repository/EmployeeRepository.java
+++ src/main/java/com/akhil/springlearning/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.akhil.springlearning.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.akhil.springlearning.entity.Employee;
@@ -9,4 +11,6 @@ import com.akhil.springlearning.entity.Employee;
 public interface EmployeeRepository extends CrudRepository<Employee, Integer>
 {
 	List<Employee> findByEmployeeName(String name);
+	
+	Page<Employee> findAll(Pageable pageable);
 }
--- src/main/java/com/akhil/springlearning/service/EmployeeService.java
+++ src/main/java/com/akhil/springlearning/service/EmployeeService.java
@@ -3,6 +3,7 @@ package com.akhil.springlearning.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.akhil.springlearning.entity.Employee;
@@ -15,9 +16,9 @@ public class EmployeeService
 	@Autowired
 	public EmployeeRepository repository;
 	
-	public Iterable<Employee> getAllEmployees()
+	public Iterable<Employee> getAllEmployees(Pageable pageable)
 	{
-		return repository.findAll();
+		return repository.findAll(pageable);
 	}
 	
 	public List<Employee> getEmployeeByName(String name)
